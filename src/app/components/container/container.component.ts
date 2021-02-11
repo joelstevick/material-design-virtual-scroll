0eda8c34ebfbb83222187657deb454fd2186b024
joelstevick <joelstevick@outlook.com>
2021-02-11 08:02:46 -0500

    introduced custom strategy

--- src/app/components/container/container.component.ts
+++ src/app/components/container/container.component.ts
@@ -1,8 +1,16 @@
-import { Component, Input } from "@angular/core";
+import { ChangeDetectionStrategy, Component, Input, OnInit } from "@angular/core";
+import {FixedSizeVirtualScrollStrategy, VIRTUAL_SCROLL_STRATEGY} from '@angular/cdk/scrolling';
 
+export class CustomVirtualScrollStrategy extends FixedSizeVirtualScrollStrategy {
+  constructor() {
+    super(50, 250, 500);
+  }
+}
 @Component({
   selector: "container",
   templateUrl: "./container.component.html",
+   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [{provide: VIRTUAL_SCROLL_STRATEGY, useClass: CustomVirtualScrollStrategy}],
   styles: [
     `
       h1 {
@@ -11,6 +19,15 @@ import { Component, Input } from "@angular/core";
     `
   ]
 })
-export class ContainerComponent {
+export class ContainerComponent implements OnInit {
   index: number;
+  items: any[] = [];
+
+  ngOnInit() {
+    for (let i = 0; i < 100; i++) {
+      this.items.push({
+        i
+      });
+    }
+  }
 }
