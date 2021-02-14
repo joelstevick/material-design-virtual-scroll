46bad50dc6869b2799d236da4c417eaa2cb00831
joelstevick <joelstevick@outlook.com>
2021-02-14 15:48:26 -0500

    1 file modified

--- src/app/strategies/chat-scroll.strategy.ts
+++ src/app/strategies/chat-scroll.strategy.ts
@@ -2,14 +2,27 @@ import {
   CdkVirtualScrollViewport,
   VirtualScrollStrategy
 } from "@angular/cdk/scrolling";
-import { Observable } from "rxjs";
+import { Observable, Subject } from "rxjs";
+import { distinctUntilChanged } from "rxjs/operators";
 
 export class ChatScrollStrategy implements VirtualScrollStrategy {
-  scrolledIndexChange: Observable<number>;
+  private index$ = new Subject<number>();
+
+  scrolledIndexChange = this.index$.pipe(distinctUntilChanged());
+
+  private viewport: CdkVirtualScrollViewport | null = null;
+
+  // lifecycle hooks
   attach(viewport: CdkVirtualScrollViewport): void {
+    this.viewport = viewport;
+    this.viewport.setTotalContentSize(0);
+    this.viewport.setRenderedRange({ start: 0, end: 0 });
+    this.viewport.setRenderedContentOffset(0);
     console.log("chat-scroll.attach");
   }
   detach(): void {
+    this.index$.complete();
+    this.viewport = null;
     console.log("chat-scroll.detach");
   }
   onContentScrolled(): void {
