759467f77a725802cef6d00926815f00e74502cd
joelstevick <joelstevick@outlook.com>
2021-02-14 15:52:43 -0500

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppComponent } from "./app.component";
 import { ScrollingModule } from "@angular/cdk/scrolling";
 import { ContainerComponent } from "./components/container/container.component";
 import { ChatScrollComponent } from "./components/chat-scroll/chat-scroll.component";
+import { ChatComponent } from "./components/chat/chat.component";
 
 @NgModule({
   imports: [BrowserModule, FormsModule, ScrollingModule],
-  declarations: [AppComponent, ContainerComponent, ChatScrollComponent],
+  declarations: [
+    AppComponent,
+    ContainerComponent,
+    ChatScrollComponent,
+    ChatComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/components/chat/chat.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "chat",
+  templateUrl: "./chat.component.html",
+  styleUrls: ["./chat.component.css"]
+})
+export class ChatComponent implements OnInit {
+  ngOnInit(): void {}
+}
